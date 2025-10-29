cd380768988615f10ca9cdb66fa638d3f37940d7
Zidane-ETML <pd01wvj@eduvaud.ch>
2025-10-29 16:26:25 +0100

    feat(shoothemup.player.cs): le joueur tombe du ciel au lieu d'apparaitre sur le sol

--- Drones/Drones/Model/player.cs
+++ Drones/Drones/Model/player.cs
@@ -6,8 +6,8 @@ namespace MonkeyGame
 {
     public partial class player
     {
-        private int x;
-        private int y;
+        private int _x;
+        private int _y;
 
         private int speedx;                                 // Position en X
         private int speedy;                                 // Position en Y
@@ -17,21 +17,21 @@ namespace MonkeyGame
         public int GroundY { get; private set; }
         public int Width { get; private set; }
         public int Height { get; private set; }
-
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
         public Rectangle Hitbox { get;set; }
 
 
         public player(int X, int Y, int width, int height)
         {
-            x = X;
-            y = Y;
-            GroundY = Y;
+            _x = X;
+            _y = Y;
+            GroundY = 600;
             Width = width;
             Height = height;
         }
 
-        public int X { get { return x;} }
-        public int Y { get { return y;} }
+
         public void move(int movex, int movey)
         {
             speedx = movex;
@@ -58,18 +58,18 @@ namespace MonkeyGame
         // que 'interval' millisecondes se sont écoulées
         public void Update(int interval)
         {
-            x += speedx;
-            y += speedy;
+            _x += speedx;
+            _y += speedy;
             velocityY += 1;
-            y += velocityY;
+            _y += velocityY;
 
-            if (y >= GroundY) 
+            if (_y >= GroundY) 
             {
-                y = GroundY;
+                _y = GroundY;
                 velocityY = 0;
                 isJumping = false ;
             }
-            Hitbox = new Rectangle(x, y, Width, Height);
+            Hitbox = new Rectangle(_x, _y, Width, Height);
         }
 
     }
--- Drones/Drones/Program.cs
+++ Drones/Drones/Program.cs
@@ -17,7 +17,7 @@ namespace MonkeyGame
             // Création de la flotte de drones
             List<player> group= new List<player>();
             List<Palm_Tree> tree= new List<Palm_Tree>();
-            group.Add(new player(Beach.WIDTH / 2, Beach.HEIGHT / 2, 50, 50));
+            group.Add(new player(Beach.WIDTH / 2, 0, 50, 50));
             int space = 100;
             tree.Add(new Palm_Tree(100, GlobalHelpers.alea.Next(0, 2)));
             for (int i = 0; i < 4; i++)
